0e8fc0e34b6f002cd7f4e82306a1bf87aa139432
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2022-03-16 23:13:47 +0800

    fix failure to sync bar

--- Assets/Scripts/UIHealthBar.cs
+++ Assets/Scripts/UIHealthBar.cs
@@ -1,17 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
+
 
 public class UIHealthBar : MonoBehaviour
 {
     public Slider slider;
+    PhotonView view;
+
+    private void Start()
+    {
+        view = GetComponent<PhotonView>();
+    }
 
     public void SetMaxHealth(int maxHealth)
+    {
+        view.RPC("SetMaxHealthRPC", RpcTarget.All, maxHealth);
+    }
+
+    public void SetHealth(int health)
+    {
+        view.RPC("SetHealthRPC", RpcTarget.All, health);
+    }
+
+    [PunRPC]
+    public void SetMaxHealthRPC(int maxHealth)
     {
         slider.maxValue = maxHealth * 1f;
         slider.value = maxHealth * 1f;
     }
 
-    public void SetHealth(int health)
+    [PunRPC]
+    public void SetHealthRPC(int health)
     {
         slider.value = health * 1f;
     }
