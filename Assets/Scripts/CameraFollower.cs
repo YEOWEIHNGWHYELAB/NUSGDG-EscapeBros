7095425b99f2a6b61beab613a3f7cf6add492c12
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2022-03-17 18:10:15 +0800

    camera follower fix

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
 public class CameraFollower : MonoBehaviour
 {
     private GameObject player;
+    PhotonView view;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        view = GetComponent<PhotonView>();
     }
 
     void Awake()
@@ -20,11 +23,21 @@ public class CameraFollower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        player = FindObjectOfType<Player>().gameObject;
-
+        if (view.IsMine)
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                player = GameObject.Find("Player1");
+            } else
+            {
+                player = GameObject.Find("Player2");
+            }
+        }
+        
         // Temporary vector
         Vector3 temp = player.transform.position;
         temp.z = transform.position.z;
+
         // Assign value to Camera position
         transform.position = temp;
     }
--- Assets/Scripts/SpawnPlayers.cs
+++ Assets/Scripts/SpawnPlayers.cs
@@ -1,16 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
+
 
 public class SpawnPlayers : MonoBehaviour
 {
     public GameObject player;
+    private bool isMaster;
     public float minX, minY, maxX, maxY;
 
     private void Start()
     {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            isMaster = true;
+        }
+        else
+        {
+            isMaster = false;
+        }
+
         Vector2 randomPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
-        PhotonNetwork.Instantiate(player.name, randomPosition, Quaternion.identity); // Specify name of game object inside resources folder you wanna spawn
+
+        if (isMaster)
+        {
+            PhotonNetwork.Instantiate("Player1", randomPosition, Quaternion.identity); // Specify name of game object inside resources folder you wanna spawn
+        } else
+        {
+            PhotonNetwork.Instantiate("Player2", randomPosition, Quaternion.identity);
+        }
     }
 }
