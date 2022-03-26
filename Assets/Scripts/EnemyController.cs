665faba83692dd269d4f8066ee600ddbe87cbfe8
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2022-03-26 17:34:32 +0800

    Enemy with RigidBody

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
 public class EnemyController : MonoBehaviour
 {
     public GameObject[] playersInGame = new GameObject[2];
     public GameObject[] nearestPlayer = new GameObject[1];
+    PhotonView view;
 
     private float distancePlayer1;
     private float distancePlayer2;
@@ -18,6 +21,7 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        view = GetComponent<PhotonView>();
         playersInGame[0] = GameObject.Find("Player1(Clone)");
         playersInGame[1] = GameObject.Find("Player2(Clone)");
     }
@@ -25,57 +29,60 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        distancePlayer1 = Vector2.Distance(transform.position, playersInGame[0].GetComponent<Transform>().position);
-        distancePlayer2 = Vector2.Distance(transform.position, playersInGame[1].GetComponent<Transform>().position);
-
-        if (distancePlayer1 <= 7F || distancePlayer2 <= 7F)
+        if (view.IsMine)
         {
-            if (distancePlayer1 <= distancePlayer2)
+            distancePlayer1 = Vector2.Distance(transform.position, playersInGame[0].GetComponent<Transform>().position);
+            distancePlayer2 = Vector2.Distance(transform.position, playersInGame[1].GetComponent<Transform>().position);
+
+            if (distancePlayer1 <= 7F || distancePlayer2 <= 7F)
             {
-                if (distancePlayer1 <= 1F)
+                if (distancePlayer1 <= distancePlayer2)
                 {
-                    if (currentDamageTimeDelta <= 0)
+                    if (distancePlayer1 <= 1F)
                     {
-                        playersInGame[0].GetComponent<PlayerHealth>().HealthControl(true, true, 20);
-                        currentDamageTimeDelta = damagePeriod;
-                    }
-                    else
-                    {
-                        currentDamageTimeDelta -= Time.deltaTime;
-                    }
+                        if (currentDamageTimeDelta <= 0)
+                        {
+                            playersInGame[0].GetComponent<PlayerHealth>().HealthControl(true, true, 20);
+                            currentDamageTimeDelta = damagePeriod;
+                        }
+                        else
+                        {
+                            currentDamageTimeDelta -= Time.deltaTime;
+                        }
                         
-                }
-                else
-                {
-                    currentDamageTimeDelta = damagePeriod;
-                }
-
-                targetX.x = playersInGame[0].GetComponent<Transform>().position.x;
-                targetX.y = transform.position.y;
-                transform.position = Vector2.MoveTowards(transform.position, targetX, speed * Time.deltaTime);
-            }
-            else
-            {
-                if (distancePlayer2 <= 1F)
-                {
-                    if (currentDamageTimeDelta <= 0)
-                    {
-                        playersInGame[1].GetComponent<PlayerHealth>().HealthControl(true, false, 20);
-                        currentDamageTimeDelta = damagePeriod;
                     }
                     else
                     {
-                        currentDamageTimeDelta -= Time.deltaTime;
+                        currentDamageTimeDelta = damagePeriod;
                     }
+
+                    targetX.x = playersInGame[0].GetComponent<Transform>().position.x;
+                    targetX.y = transform.position.y;
+                    transform.position = Vector2.MoveTowards(transform.position, targetX, speed * Time.deltaTime);
                 }
                 else
                 {
-                    currentDamageTimeDelta = damagePeriod;
-                }
+                    if (distancePlayer2 <= 1F)
+                    {
+                        if (currentDamageTimeDelta <= 0)
+                        {
+                            playersInGame[1].GetComponent<PlayerHealth>().HealthControl(true, false, 20);
+                            currentDamageTimeDelta = damagePeriod;
+                        }
+                        else
+                        {
+                            currentDamageTimeDelta -= Time.deltaTime;
+                        }
+                    }
+                    else
+                    {
+                        currentDamageTimeDelta = damagePeriod;
+                    }
 
-                targetX.x = playersInGame[1].GetComponent<Transform>().position.x;
-                targetX.y = transform.position.y;
-                transform.position = Vector2.MoveTowards(transform.position, targetX, speed * Time.deltaTime);
+                    targetX.x = playersInGame[1].GetComponent<Transform>().position.x;
+                    targetX.y = transform.position.y;
+                    transform.position = Vector2.MoveTowards(transform.position, targetX, speed * Time.deltaTime);
+                }
             }
         }
     }
